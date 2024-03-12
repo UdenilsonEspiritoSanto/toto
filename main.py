69a67f4433e97bbe002c5950c6cdfd0580c525fa
UdenilsonEspiritoSanto <149274299+UdenilsonEspiritoSanto@users.noreply.github.com>
2024-03-12 10:56:09 -0300

    criar banco

---
+++ main.py
@@ -0,0 +1,63 @@
+import flet as ft
+import sqlite3
+class Todo:
+    def __init__(self,page:ft.Page):
+        self.page = page
+        self.page.bgcolor = ft.colors.WHITE
+        self.page.window_width=400
+        self.page.window_height=450
+        self.page.window_resizable = False
+        self.page.window_always_on_top = True
+        self.page.title = 'TodoApp'
+        self.db_execute('CREATE TABLE IF NOT EXISTS tasks(name,status)')
+        self.main_page()
+    
+    def db_execute(self,query,params=[]):
+         with sqlite3.connect('database.db')as con:
+              cur = con.cursor()
+              cur.execute(query,params)
+              con.commit()
+              return cur.fetchall()
+              
+         
+    def tasks_container(self):
+
+        return ft.Container(
+            height=self.page.height *0.8,
+            content=ft.Column(
+                controls=[
+                    ft.Checkbox(label='Tarefa-1',value=True)
+                ]
+            )
+        )
+    def add(self,e,input_task):
+         pass
+    def main_page(self):
+           input_task = ft.TextField(hint_text="Dgite a Tarefa",expand=True)
+
+           input_bar = ft.Row(
+                controls=[
+                     input_task,
+                     ft.FloatingActionButton(icon=ft.icons.ADD,
+                                             on_click=lambda e:self.add(e,input_task))
+                ]
+           )
+           tab = ft.Tabs(
+               selected_index=0,
+               tabs=[
+                   ft.Tab(text='Tarefas'),
+                   ft.Tab(text='Em Andamento'),
+                   ft.Tab(text='Realizadas')                                 
+                 
+                   
+               ]
+           )
+           tasks = self.tasks_container()
+           self.page.add(
+                
+                input_bar,
+                tab,
+                tasks
+
+           )
+ft.app(target=Todo)
